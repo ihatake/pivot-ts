042c092f6614dc457778e45817e1a4d74564cf53
佟晓芳 <tongxiaofang@tongxiaofangdeMacBook-Pro.local>
2019-02-11 14:17:10 +0800

    ts错误

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import { observable, autorun } from 'mobx';
+import CardItem from './CardItem';
 
 interface S {
   [propName: string]: any;
@@ -10,7 +11,26 @@ class Home extends React.Component<{}, S> {
   @observable secondsPassed = 0;
   constructor(props?: any) {
     super(props);
-    this.state = { T: null };
+    this.state = {
+      T: null,
+      CardList: [
+        {
+          title: 'first Card',
+          id: 1,
+          content: 'this is first Card',
+        },
+        {
+          title: 'second Card',
+          id: 2,
+          content: 'this is second Card',
+        },
+        {
+          title: 'Third Card',
+          id: 3,
+          content: 'this is Third Card',
+        },
+      ],
+    };
     autorun(() => {
       console.log(this.secondsPassed);
     });
@@ -19,7 +39,7 @@ class Home extends React.Component<{}, S> {
     this.setState({
       T: setTimeout(() => {
         this.secondsPassed++;
-      }, 1000)
+      }, 1000),
     });
   }
   componentWillUnmount() {
@@ -27,9 +47,12 @@ class Home extends React.Component<{}, S> {
   }
   public render() {
     return (
-      <div className="Home">
-        Homeaaa
-        <span>Seconds passed: {this.secondsPassed} </span>
+      <div className="card">
+        {this.state.CardList.map((item: any, index: any) => {
+          return (
+            <CardItem key={index} title={item.title} content={item.content} />
+          );
+        })}
       </div>
     );
   }
